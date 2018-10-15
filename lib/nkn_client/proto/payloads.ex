b9166a1f417b5afcbef7faae65699dea88bf6e30
Ole Andre Birkedal <birkedal@extab.net>
2018-10-14 17:40:22 -0700

    Proper encoding of :TEXT payloads with the TextData field

--- lib/nkn_client/proto/payloads.ex
+++ lib/nkn_client/proto/payloads.ex
@@ -1,6 +1,7 @@
 defmodule NknClient.Proto.Payloads do
   use Protobuf, from: Path.expand("payloads.proto", __DIR__)
   alias NknClient.Proto.Payloads.Payload
+  alias NknClient.Proto.Payloads.TextData
 
   def binary(data) do
     Payload.new(type: :BINARY, data: data)
@@ -8,8 +9,12 @@ defmodule NknClient.Proto.Payloads do
   end
 
   def text(data) do
-    Payload.new(type: :TEXT, data: data)
+    TextData.new(text: data)
+    |> TextData.encode
+    |> create_payload
     |> Payload.encode
   end
+
+  defp create_payload(data), do: Payload.new(type: :TEXT, data: data)
 end
 
