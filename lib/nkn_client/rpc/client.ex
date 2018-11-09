2ef0e4b65fc555092a41d7fd10135cba419f2fce
Ole Andre Birkedal <birkedal@extab.net>
2018-11-09 15:01:02 +0100

    Change default RPC

--- lib/nkn_client/rpc/client.ex
+++ lib/nkn_client/rpc/client.ex
@@ -1,8 +1,7 @@
 defmodule NknClient.RPC.Client do
   alias JSONRPC2.Clients.HTTP
 
-  #@default_url "http://testnet-node-0001.nkn.org:30003"
-  @default_url "http://sia.anoncat.com:30003"
+  @default_url "http://testnet-node-0001.nkn.org:30003"
 
   def get_ws_address() do
     case Application.get_env(:nkn_client, :rpc_url) do
