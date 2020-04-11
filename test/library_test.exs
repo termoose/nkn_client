87b8557df13fea6b146ec67d110919e04e47a5b2
blindingdark <blindingdark@outlook.com>
2020-04-11 15:42:52 +0800

    fix tests.

--- test/library_test.exs
+++ test/library_test.exs
@@ -1,7 +1,19 @@
 defmodule NknClientTest.LibraryTest do
   use ExUnit.Case
 
+  defmodule NknClientTest.Library do
+    use NknClient
+
+    def start_link(state) do
+      NknClient.start_link(__MODULE__, state)
+    end
+
+    def handle_packet(packet) do
+      Logger.debug("Packet: #{inspect(packet)}")
+    end
+  end
+
   test "Start library" do
-    {:ok, pid} = User.start_link(:ok)
+    {:ok, pid} = NknClientTest.Library.start_link(:ok)
   end
 end
